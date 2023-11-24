38d3c72b756c476b05194e1857ceb20e8ddcd681
Oleg Nikitin <asrifox@yandex.ru>
2023-11-24 19:51:24 +0300

    Added line breaks between top-level keys

--- grc/core/io/yaml.py
+++ grc/core/io/yaml.py
@@ -8,6 +8,9 @@ from ruamel.yaml import YAML
 from .yaml_grc import typ, init_typ, ListFlowing, OrderedDictFlowing
 from ruamel.yaml.scalarstring import LiteralScalarString as MultiLineString
 
+from collections import OrderedDict
+from ruamel.yaml.comments import CommentedMap
+
 __all__ = ['dump', 'safe_load',
            'ListFlowing', 'OrderedDictFlowing', 'MultiLineString']
 
@@ -20,6 +23,17 @@ class GRCYAML(YAML):
 
 
 def dump(data, stream, default_flow_style=False, indent=4):
+    if isinstance(data, dict) or isinstance(data, OrderedDict):
+        data = CommentedMap(data)
+    elif not isinstance(data, CommentedMap):
+        raise TypeError(f'Expected mapping, found {type(data)}')
+    
+    # Add line breaks between top-level keys
+    keys = iter(data.keys())
+    next(keys)
+    for key in keys:
+        data.yaml_set_comment_before_after_key(key, before='\n\n')
+
     with GRCYAML(output=stream) as yaml:
         yaml.default_flow_style = default_flow_style
         yaml.indent = indent
